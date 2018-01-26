126804fda628ccbd018eb99aa78dc4d797401656
Nicholas Niro <blowfist@xroutine.net>
2018-01-26 00:48:33 -0500

    player: updated the license years.

--- player.sh
+++ player.sh
@@ -2,7 +2,7 @@
 
 #The MIT License (MIT)
 #
-#Copyright (c) 2010-2016 Nicholas Niro
+#Copyright (c) 2010-2018 Nicholas Niro
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
